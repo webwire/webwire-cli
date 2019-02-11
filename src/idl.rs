05e125b3525b8353c34a5a4fd9d2955df764ef4b
Michael P. Jung <michael.jung@terreon.de>
2019-02-11 18:09:20 +0100

    Improve IDL parsing using CompleteStr

--- src/idl.rs
+++ src/idl.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use nom::types::CompleteStr;
 
 #[derive(Debug, PartialEq)]
 pub struct Field {
@@ -12,37 +12,52 @@ pub struct Service {
     pub fields: Vec<Field>,
 }
 
-named!(parse_field<&str, Field>, do_parse!(
-    name: take_while1!(char::is_alphanumeric) >>
+named!(parse_identifier<CompleteStr, String>, do_parse!(
+    first: take_while1!(char::is_alphabetic) >>
+    rest: take_while!(char::is_alphanumeric) >>
+    (first.0.to_owned() + rest.0)
+));
+
+named!(parse_field<CompleteStr, Field>, do_parse!(
+    name: parse_identifier >>
     take_while!(char::is_whitespace) >>
-    tag!(":") >>
+    char!(':') >>
     take_while!(char::is_whitespace) >>
-    type_: take_while1!(char::is_alphanumeric) >>
+    type_: parse_identifier >>
     (Field { name: name.to_string(), type_: type_.to_string() })
 ));
 
-named!(parse_field_separator<&str, ()>, do_parse!(
+named!(parse_field_separator<CompleteStr, ()>, do_parse!(
     take_while!(char::is_whitespace) >>
-    tag!(",") >>
+    char!(',') >>
     take_while!(char::is_whitespace) >>
     ()
 ));
 
-named!(parse_service<&str, Service>, do_parse!(
+named!(parse_service<CompleteStr, Service>, do_parse!(
         tag!("service") >>
         take_while1!(char::is_whitespace) >>
-        name: take_while1!(char::is_alphanumeric) >>
+        name: parse_identifier >>
         // FIXME parse fields
         take_while!(char::is_whitespace) >>
         char!('{') >>
         take_while!(char::is_whitespace) >>
         fields: separated_list!(parse_field_separator, parse_field) >>
         take_while!(char::is_whitespace) >>
-        opt!(tag!(",")) >> // trailing comma
+        opt!(char!(',')) >> // trailing comma
         take_while!(char::is_whitespace) >>
         char!('}') >>
         (Service { name: name.to_string(), fields: fields })
 ));
+
+#[test]
+fn test_parse_identifier() {
+    assert_eq!(
+        parse_identifier(CompleteStr("test")),
+        Ok((CompleteStr(""), "test".to_string()))
+    );
+}
+
 
 #[test]
 fn test_parse_service() {
@@ -54,9 +69,9 @@ fn test_parse_service() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_service(content),
-            Ok(("", Service {
-                name: "Pinger".to_string(),
+            parse_service(CompleteStr(content)),
+            Ok((CompleteStr(""), Service {
+                name: CompleteStr("Pinger").to_string(),
                 fields: vec![],
             }))
         );
@@ -72,8 +87,8 @@ fn test_parse_field() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_field(content),
-            Ok(("", Field {
+            parse_field(CompleteStr(content)),
+            Ok((CompleteStr(""), Field {
                 name: "foo".to_string(),
                 type_: "FooType".to_string(),
             }))
@@ -101,13 +116,10 @@ fn test_parse_service_with_fields() {
         // all combined
         "service Pinger { request: Ping , response: Pong , }",
     ];
-    let mut fields = BTreeMap::<String, String>::new();
-    fields.insert("request".into(), "Ping".into());
-    fields.insert("response".into(), "Pong".into());
     for content in contents.iter() {
         assert_eq!(
-            parse_service(content),
-            Ok(("", Service {
+            parse_service(CompleteStr(content)),
+            Ok((CompleteStr(""), Service {
                 name: "Pinger".to_string(),
                 fields: vec![
                     Field { name: "request".to_string(), type_: "Ping".to_string() },
