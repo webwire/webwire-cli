56d5910b91f038fbfb25dddb7065d9d0b85bb6fb
Michael P. Jung <michael.jung@terreon.de>
2019-09-15 18:39:59 +0200

    Update nom to version 5

--- src/idl.rs
+++ src/idl.rs
@@ -1,4 +1,17 @@
-use nom::types::CompleteStr;
+use nom::{
+    IResult,
+    branch::alt,
+    bytes::complete::{tag, take_while, take_while1},
+    character::complete::{
+        alpha1,
+        alphanumeric0,
+    },
+    combinator::{
+        opt,
+    },
+    error::ErrorKind,
+    multi::separated_list
+};
 
 #[derive(Debug, PartialEq)]
 pub struct Field {
@@ -8,77 +21,157 @@ pub struct Field {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Type {
+pub struct Struct {
     pub name: String,
     pub fields: Vec<Field>,
 }
 
 #[derive(Debug, PartialEq)]
+pub struct FieldsetField {
+    pub name: String,
+    pub optional: bool
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Fieldset {
+    pub name: String,
+    pub fields: Vec<FieldsetField>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Function {
+    pub name: String,
+    pub request: String,
+    pub response: String
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Service {
     pub name: String,
+    // FIXME
     pub fields: Vec<Field>,
 }
 
-named!(parse_identifier<CompleteStr, String>, do_parse!(
-    first: take_while1!(char::is_alphabetic) >>
-    rest: take_while!(char::is_alphanumeric) >>
-    (first.0.to_owned() + rest.0)
-));
+#[derive(Debug, PartialEq)]
+pub enum DocumentPart {
+    Struct(Struct),
+    Fieldset(Fieldset),
+    Function(Function),
+    Service(Service)
+}
 
-named!(parse_field<CompleteStr, Field>, do_parse!(
-    name: parse_identifier >>
-    take_while!(char::is_whitespace) >>
-    optional: opt!(char!('?')) >>
-    take_while!(char::is_whitespace) >>
-    char!(':') >>
-    take_while!(char::is_whitespace) >>
-    type_: parse_identifier >>
-    (Field { name: name.to_string(), type_: type_.to_string(), optional: optional != None })
-));
+#[derive(Debug, PartialEq)]
+pub struct Document {
+    pub parts: Vec<DocumentPart>
+}
 
-named!(parse_field_separator<CompleteStr, ()>, do_parse!(
-    take_while!(char::is_whitespace) >>
-    char!(',') >>
-    take_while!(char::is_whitespace) >>
-    ()
-));
+fn parse_identifier(input: &str) -> IResult<&str, String> {
+    let (input, first) = alpha1(input)?;
+    let (input, rest) = alphanumeric0(input)?;
+    Ok((input, String::from(first) + rest))
+}
 
-named!(parse_fields<CompleteStr, Vec<Field>>, do_parse!(
-        take_while!(char::is_whitespace) >>
-        char!('{') >>
-        take_while!(char::is_whitespace) >>
-        fields: separated_list!(parse_field_separator, parse_field) >>
-        take_while!(char::is_whitespace) >>
-        opt!(char!(',')) >> // trailing comma
-        take_while!(char::is_whitespace) >>
-        char!('}') >>
-        (fields)
-));
+fn parse_field(input: &str) -> IResult<&str, Field> {
+    let (input, name) = parse_identifier(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, optional) = opt(tag("?"))(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, type_) = parse_identifier(input)?;
+    Ok((input, Field {
+        name: name.to_string(),
+        type_: type_.to_string(),
+        optional: optional != None
+    }))
+}
 
-named!(parse_type<CompleteStr, Type>, do_parse!(
-        tag!("type") >>
-        take_while1!(char::is_whitespace) >>
-        name: parse_identifier >>
-        take_while!(char::is_whitespace) >>
-        fields: parse_fields >>
-        (Type { name: name.to_string(), fields: fields })
-));
+fn parse_field_separator(input: &str) -> IResult<&str, ()> {
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, _) = tag(",")(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    Ok((input, ()))
+}
 
-named!(parse_service<CompleteStr, Service>, do_parse!(
-        tag!("service") >>
-        take_while1!(char::is_whitespace) >>
-        name: parse_identifier >>
-        take_while!(char::is_whitespace) >>
-        fields: parse_fields >>
-        (Service { name: name.to_string(), fields: fields })
-));
+fn parse_fields(input: &str) -> IResult<&str, Vec<Field>> {
+    let (input, _) = tag("{")(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, fields) = separated_list(parse_field_separator, parse_field)(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, _) = opt(tag(","))(input)?; // trailing comma
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, _) = tag("}")(input)?;
+    Ok((input, fields))
+}
 
+fn parse_type(input: &str) -> IResult<&str, Struct> {
+    let (input, _) = tag("type")(input)?;
+    let (input, _) = take_while1(char::is_whitespace)(input)?;
+    let (input, name) = parse_identifier(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, fields) = parse_fields(input)?;
+    Ok((input, Struct {
+        name: name.to_string(),
+        fields: fields
+    }))
+}
+
+fn parse_service(input: &str) -> IResult<&str, Service> {
+    let (input, _) = tag("service")(input)?;
+    let (input, _) = take_while1(char::is_whitespace)(input)?;
+    let (input, name) = parse_identifier(input)?;
+    let (input, _) = take_while(char::is_whitespace)(input)?;
+    let (input, fields) = parse_fields(input)?;
+    Ok((input, Service {
+        name: name.to_string(),
+        fields: fields
+    }))
+}
+
+/* FIXME
+pub fn parse_document_part(input: &str) -> IResult<&str, DocumentPart> {
+    let (input, part) = alt((
+        parse_struct,
+        parse_fieldset,
+    ))(input);
+    Ok((input, part))
+}
+
+pub fn parse_document(input: &str) -> IResult<&str, Document> {
+    let (input, values) = many0(
+        alt(
+            parse_struct,
+            // TODO parse function
+            // TODO parse
+            parse_service,
+        )
+    )
+    (Document {
+    })
+}
+*/
 
 #[test]
 fn test_parse_identifier() {
     assert_eq!(
-        parse_identifier(CompleteStr("test")),
-        Ok((CompleteStr(""), "test".to_string()))
+        parse_identifier("test"),
+        Ok(("", "test".to_string()))
+    );
+    assert_eq!(
+        parse_identifier("test123"),
+        Ok(("", "test123".to_string()))
+    );
+}
+
+#[test]
+fn test_parse_identifier_invalid() {
+    assert_eq!(
+        parse_identifier("123test"),
+        Err(nom::Err::Error(("123test", ErrorKind::Alpha)))
+    );
+    assert_eq!(
+        parse_identifier("_test"),
+        Err(nom::Err::Error(("_test", ErrorKind::Alpha)))
     );
 }
 
@@ -91,8 +184,8 @@ fn test_parse_field() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_field(CompleteStr(content)),
-            Ok((CompleteStr(""), Field {
+            parse_field(content),
+            Ok(("", Field {
                 name: "foo".to_string(),
                 type_: "FooType".to_string(),
                 optional: false
@@ -111,12 +204,78 @@ fn test_parse_optional_field() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_field(CompleteStr(content)),
-            Ok((CompleteStr(""), Field {
+            parse_field(content),
+            Ok(("", Field {
                 name: "foo".to_string(),
                 type_: "FooType".to_string(),
                 optional: true
             }))
+        );
+    }
+}
+
+#[test]
+fn test_parse_fields_0() {
+    let contents = [
+        "{}",
+        "{ }",
+        "{,}",
+        "{ ,}",
+        "{, }",
+    ];
+    for content in contents.iter() {
+        assert_eq!(
+            parse_fields(content),
+            Ok(("", vec![]))
+        );
+    }
+}
+
+#[test]
+fn test_parse_fields_1() {
+    let contents = [
+        "{foo:Foo}",
+        "{foo: Foo}",
+        "{foo:Foo }",
+        "{ foo:Foo}",
+        "{foo:Foo,}"
+    ];
+    for content in contents.iter() {
+        assert_eq!(
+            parse_fields(content),
+            Ok(("", vec![Field {
+                name: "foo".to_owned(),
+                type_: "Foo".to_owned(),
+                optional: false
+            }]))
+        );
+    }
+}
+
+#[test]
+fn test_parse_fields_2() {
+    let contents = [
+        "{foo:Foo,bar:Bar}",
+        "{foo: Foo, bar: Bar}",
+        "{ foo:Foo,bar:Bar }",
+        "{ foo: Foo, bar: Bar }",
+        "{ foo: Foo, bar: Bar, }",
+    ];
+    for content in contents.iter() {
+        assert_eq!(
+            parse_fields(content),
+            Ok(("", vec![
+                Field {
+                    name: "foo".to_owned(),
+                    type_: "Foo".to_owned(),
+                    optional: false
+                },
+                Field {
+                    name: "bar".to_owned(),
+                    type_: "Bar".to_owned(),
+                    optional: false
+                }
+            ]))
         );
     }
 }
@@ -131,9 +290,9 @@ fn test_parse_type() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_type(CompleteStr(content)),
-            Ok((CompleteStr(""), Type {
-                name: CompleteStr("Pinger").to_string(),
+            parse_type(content),
+            Ok(("", Struct {
+                name: "Pinger".to_string(),
                 fields: vec![],
             }))
         );
@@ -141,13 +300,10 @@ fn test_parse_type() {
 }
 
 #[test]
-fn test_parse_invalid_type() {
+fn test_parse_type_invalid() {
     assert_eq!(
-        parse_type(CompleteStr("type 123fail{}")),
-        Ok((CompleteStr(""), Type {
-            name: "foo".to_string(),
-            fields: vec![],
-        }))
+        parse_type("type 123fail{}"),
+        Err(nom::Err::Error(("123fail{}", ErrorKind::Alpha)))
     )
 }
 
@@ -175,8 +331,8 @@ fn test_parse_type_with_fields() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_type(CompleteStr(content)),
-            Ok((CompleteStr(""), Type {
+            parse_type(content),
+            Ok(("", Struct {
                 name: "Person".to_string(),
                 fields: vec![
                     Field { name: "name".to_string(), type_: "String".to_string(), optional: false },
@@ -211,8 +367,8 @@ fn test_parse_service_with_fields() {
     ];
     for content in contents.iter() {
         assert_eq!(
-            parse_service(CompleteStr(content)),
-            Ok((CompleteStr(""), Service {
+            parse_service(content),
+            Ok(("", Service {
                 name: "Pinger".to_string(),
                 fields: vec![
                     Field { name: "request".to_string(), type_: "Ping".to_string(), optional: false },
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate serde_derive;
 
-#[macro_use]
 extern crate nom;
 
 pub mod schema;
