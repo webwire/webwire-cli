dab3b197b935b0c4061287514f26903db9b5ce1d
Michael P. Jung <michael.jung@terreon.de>
2020-06-30 17:37:02 +0200

    Improve rust schema generation

--- src/codegen/rust.rs
+++ src/codegen/rust.rs
@@ -18,10 +18,7 @@ fn optional(stream: TokenStream) -> TokenStream {
 pub fn generate(doc: &schema::Document) -> TokenStream {
     let namespace = gen_namespace(&doc.ns);
     quote! {
-        use async_trait::async_trait;
-
-        use webwire;
-
+        #[allow(dead_code)]
         #namespace
     }
 }
@@ -40,7 +37,7 @@ fn gen_namespace(ns: &schema::Namespace) -> TokenStream {
         let child_ns_name = quote::format_ident!("{}", child_ns.name());
         let child_ns_stream = gen_namespace(child_ns);
         stream.extend(quote! {
-            mod #child_ns_name {
+            pub mod #child_ns_name {
                 #child_ns_stream
             }
         });
@@ -60,7 +57,8 @@ fn gen_enum(enum_: &schema::Enum) -> TokenStream {
     let name = quote::format_ident!("{}", &enum_.fqtn.name);
     let variants = gen_enum_variants(enum_);
     quote! {
-        enum #name {
+        #[derive(Clone, Debug, Eq, PartialEq, ::serde::Serialize, ::serde::Deserialize)]
+        pub enum #name {
             #variants
         }
     }
@@ -92,6 +90,7 @@ fn gen_struct(struct_: &schema::Struct) -> TokenStream {
     let name = quote::format_ident!("{}", &struct_.fqtn.name);
     let fields = gen_struct_fields(struct_);
     quote! {
+        #[derive(Clone, Debug, Eq, PartialEq, ::serde::Serialize, ::serde::Deserialize)]
         pub struct #name {
             #fields
         }
@@ -121,6 +120,7 @@ fn gen_fieldset(fieldset: &schema::Fieldset) -> TokenStream {
     let name = quote::format_ident!("{}", &fieldset.fqtn.name);
     let fields = gen_fieldset_fields(fieldset);
     quote! {
+        #[derive(Clone, Debug, Eq, PartialEq, ::serde::Serialize, ::serde::Deserialize)]
         pub struct #name {
             #fields
         }
@@ -150,7 +150,7 @@ fn gen_service(service: &schema::Service) -> TokenStream {
     let name = quote::format_ident!("{}", &service.name);
     let methods = gen_service_methods(&service);
     quote! {
-        #[async_trait]
+        #[async_trait::async_trait]
         pub trait #name {
             #methods
         }
@@ -163,14 +163,14 @@ fn gen_service_methods(service: &schema::Service) -> TokenStream {
         let name = quote::format_ident!("{}", method.name);
         let input = match &method.input {
             Some(type_) => gen_typeref(type_),
-            None => quote! {},
+            None => quote! { () },
         };
         let output = match &method.output {
             Some(type_) => gen_typeref(type_),
             None => quote! { () },
         };
         stream.extend(quote! {
-            async fn #name(&self, request: &webwire::Request<#input>) -> webwire::Response<#output>;
+            async fn #name(&self, request: &::webwire::Request<#input>) -> ::webwire::Response<#output>;
         })
     }
     stream
@@ -182,7 +182,7 @@ fn gen_typeref(type_: &schema::Type) -> TokenStream {
         schema::Type::Integer => quote! { i64 },
         schema::Type::Float => quote! { f64 },
         schema::Type::String => quote! { String },
-        schema::Type::UUID => quote! { UUID },
+        schema::Type::UUID => quote! { ::uuid::Uuid },
         schema::Type::Date => quote! { Date },
         schema::Type::Time => quote! { Time },
         schema::Type::DateTime => quote! { DateTime },
@@ -190,14 +190,14 @@ fn gen_typeref(type_: &schema::Type) -> TokenStream {
         schema::Type::Array(array) => {
             let item_type = gen_typeref(&array.item_type);
             quote! {
-                Vec<#item_type>
+                std::vec::Vec<#item_type>
             }
         }
         schema::Type::Map(map) => {
             let key_type = gen_typeref(&map.key_type);
             let value_type = gen_typeref(&map.value_type);
             quote! {
-                Map<#key_type, #value_type>
+                std::collections::HashMap<#key_type, #value_type>
             }
         }
         // named
